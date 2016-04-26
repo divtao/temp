112fde51db4f6a2a2a32fd312a96d346f465e478
blogtao <guohaitao93@sina.com>
2016-04-26 08:21:55 +0800

    Initial Commit

---
+++ temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by guohaitao on 16/4/26.
+//  Copyright © 2016年 guohaitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ temp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  temp
+//
+//  Created by guohaitao on 16/4/26.
+//  Copyright © 2016年 guohaitao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
